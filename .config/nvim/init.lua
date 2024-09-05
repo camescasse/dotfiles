92ff38def2a2267d5c6bf0920a1dd87e0fc84428
camescasse <e.camescasse@gmail.com>
2024-09-05 16:55:00 -0400

    fix nvim-lsp breaking change

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -502,7 +502,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        ts_ls = {},
         cssls = {},
         docker_compose_language_service = {},
         dockerls = {},
