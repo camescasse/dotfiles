9b3714dcedf7367bb0a1bd1c63d37ecbdcf65ca8
camescasse <e.camescasse@gmail.com>
2024-09-06 11:19:06 -0400

    update init.lua to fix tsserver deprecation temporarily

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -499,7 +499,7 @@ require('lazy').setup({
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
         tsserver = {},
         cssls = {},
         docker_compose_language_service = {},
@@ -546,9 +546,12 @@ require('lazy').setup({
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
+            if server_name == 'tsserver' then
+              server_name = 'ts_ls'
+            end
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
